5e0bfb57216e575ee16b556d1ad28e0ee451cebb
Jack Firth <jackhfirth@gmail.com>
2015-07-06 23:54:24 -0700

    Add simple test scaffold

---
+++ test-suite/Spec.hs
@@ -0,0 +1,16 @@
+import           Control.Exception (evaluate)
+import           Test.Hspec
+import           Test.QuickCheck
+
+
+main :: IO ()
+main = hspec $
+  describe "Prelude.head" $ do
+    it "returns the first element of a list"
+      (head [23 ..] `shouldBe` (23 :: Int))
+
+    it "returns the first element of an *arbitrary* list"
+      (property $ \x xs -> head (x:xs) == (x :: Int))
+
+    it "throws an exception if used with an empty list"
+      (evaluate (head []) `shouldThrow` anyException)
