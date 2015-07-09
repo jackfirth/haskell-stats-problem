ba863411a3b87d5ff0a467ca2fd7f747abd9a9a7
Jack Firth <jackhfirth@gmail.com>
2015-07-09 12:40:56 -0700

    Create rudimentary column header parsing

--- Main.hs
+++ Main.hs
@@ -1,8 +1,26 @@
+import           Data.List.Split
 import           System.Process
 
 main :: IO ()
-main = generateRaw 3 >>= putStrLn
+main = do
+  raw <- generateRaw 3
+  print (rawGeneratedDataHeader raw)
 
 
 generateRaw :: Integer -> IO String
 generateRaw n = readProcess "./generator" [show n] ""
+
+data ColumnHeader = ColumnHeader String String deriving Show
+
+rawGeneratedDataHeader :: String -> [ColumnHeader]
+rawGeneratedDataHeader = map parseHeader . splitRawColumnInfo . firstLine
+
+
+firstLine :: String -> String
+firstLine = head . lines
+
+splitRawColumnInfo :: String -> [String]
+splitRawColumnInfo = splitOn ","
+
+parseHeader :: String -> ColumnHeader
+parseHeader _ = error "not implemented"
