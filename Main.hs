b122591c1a1e472d0ebf38be74e9b0239a3ae85e
Jack Firth <jackhfirth@gmail.com>
2015-07-11 18:07:50 -0700

    Refactoring

--- Main.hs
+++ Main.hs
@@ -11,16 +11,14 @@ main = do
   let headers = rawGeneratedDataHeaders raw
   let rows = rawGeneratedDataToRows headers raw
   let columns = dataRowsToColumns headers rows
-  let counts = map columnCount columns
-  print ("Counts: " ++ show counts)
-  let nullCounts = map columnNullCount columns
-  print ("Null counts: " ++ show nullCounts)
-  let numMins = mapOverNumColumns columnMin columns
-  print ("Number column minimums: " ++ show numMins)
-  let numMaxs = mapOverNumColumns columnMax columns
-  print ("Number column maximums: " ++ show numMaxs)
-  let numAverages = mapOverNumColumns columnAverage columns
-  print ("Number column averages: " ++ show numAverages)
+  printColumnsStat (map columnCount) "Counts" columns
+  printColumnsStat (map columnNullCount) "Null counts" columns
+  printColumnsStat (mapOverNumColumns columnMin) "Number column minimums" columns
+  printColumnsStat (mapOverNumColumns columnMax) "Number column maximums" columns
+  printColumnsStat (mapOverNumColumns columnAverage) "Number column averages" columns
+
+printColumnsStat :: Show a => ([DataColumn] -> a) -> String -> [DataColumn] -> IO ()
+printColumnsStat f description columns = print (description ++ ": " ++ show (f columns))
 
 generateRaw :: Integer -> IO String
 generateRaw n = readProcess "./generator" [show n] ""
